2e22124bee75d9d6d493107cb0a575f840dca193
CafeTaKuMi <43571243+CafeTaKuMi@users.noreply.github.com>
2024-12-23 17:21:01 +0700

    test: antd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
+"use client";
+import { Button, Card } from "antd";
 import Image from "next/image";
+const { Meta } = Card;
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Button type="primary">Button</Button>
+        <Card
+          hoverable
+          style={{ width: 240 }}
+          cover={
+            <img
+              alt="example"
+              src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+            />
+          }
+        >
+          <Meta title="Europe Street beat" description="www.instagram.com" />
+        </Card>
         <Image
           className="dark:invert"
           src="/next.svg"
